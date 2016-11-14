78f721a3452641539b38e0d92a2e4080ce676378
sunchenchen <15326922@qq.com>
2016-11-14 23:43:37 +0800

    fregmenttest
    13.[上机实践]横屏和竖屏

---
+++ fregmenttest/src/main/java/com/example/fregmenttest/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fregmenttest;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.fregmenttest.dummy.WordsContent;
+
+public class MainActivity extends AppCompatActivity implements
+        ItemFragment.OnListFragmentInteractionListener,DetailFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(WordsContent.WordItem wordItem) {
+        Bundle arguments =new Bundle();
+        arguments.putString("id",wordItem.id);
+        DetailFragment fragment=new DetailFragment();
+        fragment.setArguments(arguments);
+        getFragmentManager().beginTransaction().replace(R.id.worddetail,fragment).commit();
+
+    }
+}
